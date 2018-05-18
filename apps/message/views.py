09fcdd3ec001aa9ea49971a008933865dec0c2da
shenyoujian <shenyoujian2.163.com>
2018-05-18 10:52:54 +0800

    3-5

--- apps/message/views.py
+++ apps/message/views.py
@@ -6,22 +6,11 @@ from message.models import UserMessage
 
 
 def getform(request):
-    all_message = UserMessage.objects.filter(name="shenyoujian2")
-    all_message.delete()
-    for message in all_message:
-        message.delete()
-    if request.method == 'POST':
-        name = request.POST.get('name','')
-        message = request.POST.get('message','')
-        address = request.POST.get('address','')
-        email = request.POST.get('email','')
-
-        user_message = UserMessage()
-        user_message.name = name
-        user_message.message = message
-        user_message.address = address
-        user_message.email = email
-        user_message.object_id = 'lss'
-        user_message.save()
-    return render(request, 'message_form.html')
+    message = None
+    all_message = UserMessage.objects.filter(name="shen")
+    if all_message:
+        message = all_message[0]
+    return render(request, 'message_form.html',{
+        "my_message":message
+    })
 
